9fa7d929c31321eecc5c00f905b022465fe32e04
Alex <kontakt@a-wels.de>
2024-01-08 14:47:28 +0100

    run every hour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 from util import book, storygraph, moonplus
+import schedule
+import time
 
 
 def main():
@@ -15,10 +17,20 @@ def main():
     args = parser.parse_args()
     syncer = storygraph.StoryGraphSyncer()
 
+    schedule.every().hour.do(task, args, syncer)
+    schedule.run_all()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def task(args, syncer):
     # get list of books from moon+ reader cache files
+    print("[{}] SYNCING....".format(time.strftime("%H:%M:%S")))
     books = moonplus.get_moonplus_books(
         args.path)
     syncer.sync(books)
+    print("[{}] GOING BACK TO SLEEP....".format(time.strftime("%H:%M:%S")))
 
 
 if __name__ == '__main__':
